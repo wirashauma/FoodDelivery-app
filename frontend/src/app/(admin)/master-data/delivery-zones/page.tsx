324db2c7d6f818010abc33a65b23e135a1a65d7f
wirashauma <wirashaumaa@gmail.com>
2026-01-29 13:06:31 +0700

    feat: add DeliveryZonesPage to redirect to master data with zones tab active

---
+++ frontend/src/app/(admin)/master-data/delivery-zones/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { redirect } from 'next/navigation';
+
+export default function DeliveryZonesPage() {
+  // Redirect to master data with zones tab active
+  redirect('/master-data/categories?tab=zones');
+}
