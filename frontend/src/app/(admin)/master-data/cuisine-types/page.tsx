f7ba2fe65c703e7b138c57791e4f0e8aafc3c189
wirashauma <wirashaumaa@gmail.com>
2026-01-29 13:06:18 +0700

    feat: add CuisineTypesPage to redirect to master data with cuisine tab active

---
+++ frontend/src/app/(admin)/master-data/cuisine-types/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { redirect } from 'next/navigation';
+
+export default function CuisineTypesPage() {
+  // Redirect to master data with cuisine tab active
+  redirect('/master-data/categories?tab=cuisines');
+}
